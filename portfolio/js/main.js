e8cee8c6f4b78f7f184053de96c49cb86b486c25
ClauMndz <claudiamendezt19@gmail.com>
2024-01-03 20:25:33 -0500

    Refactor code for improved performance

---
+++ portfolio/js/main.js
@@ -0,0 +1,42 @@
+// Menú
+const hamburger = document.querySelector('.hamburger');
+const navList = document.querySelector('.nav ul');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    navList.classList.toggle('active');
+});
+
+// Dark-Light Mode
+const darkModeButton = document.getElementById('darkModeButton');
+const body = document.body;
+
+const enableDarkMode = () => {
+    body.classList.add('dark-mode');
+}
+
+const disableDarkMode = () => {
+    body.classList.remove('dark-mode');
+}
+
+darkModeButton.addEventListener('change', () => {
+    if (darkModeButton.checked) { 
+        disableDarkMode();
+    } else {
+        enableDarkMode();
+    }
+});
+
+// Multiple text
+const typed = new Typed('.multiple', {
+    strings: [
+        'Web Designer',
+        'Junior Front-End Developer',
+        'creative person',
+        'Freelancer'
+    ],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
