c53fe02e9557c60a06b86bc32bb997423d2c30cb
ClauMndz <claudiamendezt19@gmail.com>
2024-01-09 18:01:37 -0500

    Update index.html with new meta tags, header, and home section

---
+++ claudev__/js/main.js
@@ -0,0 +1,7 @@
+const typed = new Typed('#text', {
+    strings: ['Trainee FrontEnd Junior', 'Web Developer', 'creative person'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,    
+    loop: true,
+});
